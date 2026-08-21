9531adc946e884e4e445557ebb1a2054a885d10f
Mikhail Blinkov <mvikdvwies@gmail.com>
2026-08-21 10:19:21 +0300

    feat: enrich Post card with link and meta

--- src/features/posts/Post.jsx
+++ src/features/posts/Post.jsx
@@ -2,8 +2,22 @@ import "./Post.css";
 
 export default function Post({ post }) {
   return (
-    <div className="post">
-      <h2>{post.title}</h2>
-    </div>
+    <article className="post">
+      <a
+        href={post.url || `https://news.ycombinator.com/item?id=${post.id}`}
+        target="_blank" //открывает ссылку в новой вкладке
+        rel="noopener noreferrer" //предотвращает атаки типа clickjacking
+        className="post-link" //стили для ссылки
+      >
+        {post.title}
+      </a>
+      {/* метаданные поста */}
+      <p className="post-meta">
+        {/*очки и автор*/}
+        {post.score} points | by {post.by} |{" "}
+        {/*время публикации в локальном формате*/}
+        {new Date(post.time * 1000).toLocaleString()}{" "}
+      </p>
+    </article>
   );
 }
